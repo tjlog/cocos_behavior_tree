075d65c94bcd4b032e9745b3202762d5a6fc47d1
liuxin <963875368@qq.com>
2021-03-26 16:09:59 +0800

    完善

--- assets/scripts/lib/behavior_tree/core/BlackBoard.ts
+++ assets/scripts/lib/behavior_tree/core/BlackBoard.ts
@@ -1,11 +1,36 @@
+import TNodeBase from "../base/TNodeBase";
+
 /**
  * 用具节点之间数据的分享
  */
 export default class BlackBoard {
-    private _baseMemory=null;
-    private _treeMomory=null;
+    private _baseMemory:{[key:string]:any}=null;
+    private _treeMomory:{[key:string]:TreeData}=null;
     constructor(){
         this._baseMemory={}
         this._treeMomory={};
+    }
+
+    private _getTreeMemory(id:string):TreeData{
+        if(!this._treeMomory[id]){
+            this._treeMomory[id]=new TreeData();
+        }
+        return this._treeMomory[id];
+    }
+    
+}
+
+
+
+class TreeData{
+    nodeMemory:any;
+    openNodes:Array<TNodeBase>;
+    traversalDepth:number;
+    traversalCycle:number;
+    constructor(){
+        this.nodeMemory={};
+        this.openNodes=[];
+        this.traversalDepth=0;
+        this.traversalCycle=0;
     }
 }
