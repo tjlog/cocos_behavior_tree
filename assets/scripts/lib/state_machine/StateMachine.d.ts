8d624f013a68f6a4b42af6393f807448425b536c
liuxin <963875368@qq.com>
2021-03-29 16:51:41 +0800

    有限状态机

--- assets/scripts/lib/state_machine/StateMachine.d.ts
+++ assets/scripts/lib/state_machine/StateMachine.d.ts
@@ -1,1 +1,108 @@
-declare function StateMachine(data:any):void;
+declare class StateMachine {
+    constructor(data:any);
+    /**
+     * var Matter = StateMachine.factory({     //  <-- the factory is constructed here
+        init: 'solid',
+        transitions: [
+            { name: 'melt',     from: 'solid',  to: 'liquid' },
+            { name: 'freeze',   from: 'liquid', to: 'solid'  },
+            { name: 'vaporize', from: 'liquid', to: 'gas'    },
+            { name: 'condense', from: 'gas',    to: 'liquid' }
+        ]
+    });
+
+  var a = new Matter(),    //  <-- instances are constructed here
+      b = new Matter(),
+      c = new Matter();
+     * @param data 
+     */
+    declare static factory(data:any,data2?:any); 
+
+
+    /**
+     * 
+     * @param eventStr 
+     * 
+     * lifecycle event 
+     * onBeforeTransition  transition 之前执行
+     * onLeaveState 离开任何一个状态执行
+     * onTransition  transition期间
+     * onEnterState 进入一个状态
+     * onAfterTransition  transition 之后
+     * 
+     * //普通事件
+     * 
+     * onBefore<TRANSITION> 执行在Transition之前
+     * 
+     * 
+     * 
+     * 
+     *  onInvalidTransition 执行无效的transition 会触发这个事件
+     * 
+     * 
+     * @param cb 
+     */
+    observe(eventStr:string,cb:(data?:LiftCycle,...args)=>void);
+    observe(data:any);
+    observe(eventStr:string,cb:(data?:LiftCycle,...args)=>Promise<void>);
+
+
+    /**
+     * 判断该事件是否是一个有效的事件
+     * @param transition 
+     */
+    can(transition:string):boolean;
+
+
+    /**
+     * 历史
+     */
+    history:Array<string>;
+    /**
+     * 清空所有历史
+     */
+    clearHistory();
+
+
+    /**
+     * 根据历史倒退 (超过会报错)
+     */
+    historyBack();
+
+    /**
+     * 历史前进(超过会报错)
+     */
+    historyForward();
+
+    /**
+     * 是否允许历史倒退
+     */
+    canHistoryBack():boolean;
+
+    /**
+     * 是否允许历史前进
+     */
+    canHistoryForward():boolean;
+}
+
+declare class LiftCycle{
+    transition:string;//transition name 值
+    from:string
+    to:string;
+}
+
+declare class StateMachineHistory{
+    constructor(data?:{name:string});//name 是影响StateMachine 的字段 默认 history
+}
+
+
+/**
+ * 可视化
+ * 
+ * 
+ * 状态机可视化工具
+ * @param fsm 
+ * @param data 
+ */
+declare function StateMachineVisualize(fsm:any,data?:any);
+
--- assets/scripts/test/T1.ts
+++ assets/scripts/test/T1.ts
@@ -1,87 +1,224 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 @ccclass
 export default class T1 extends cc.Component {
-    onLoad(){
-       this.test3()
+    start() {
+        this.test7();
     }
 
 
     /**
      *  from --to   name的执行顺序必须要是当前的地址开始 
      */
-    test1(){
+    test1() {
         var fsm = new StateMachine({
             init: 'solid',
             transitions: [
-              { name: 'melt',     from: 'solid',  to: 'liquid' },
-              { name: 'freeze',   from: 'liquid', to: 'solid'  },
-              { name: 'vaporize', from: 'liquid', to: 'gas'    },
-              { name: 'condense', from: 'gas',    to: 'liquid' }
+                { name: 'melt', from: 'solid', to: 'liquid' },
+                { name: 'freeze', from: 'liquid', to: 'solid' },
+                { name: 'vaporize', from: 'liquid', to: 'gas' },
+                { name: 'condense', from: 'gas', to: 'liquid' }
             ]
-          });
- 
-        console.log(fsm.state); //solid
+        });
 
-        fsm.melt();   //solid-liquid
-        console.log(fsm.state); //liquid
-        fsm.vaporize(); // liquid -gas
-        console.log(fsm.state); //gas
-        fsm.condense(); //gas -liquid
-        console.log(fsm.state); //liquid
+        console.log(fsm["state"]); //solid
+
+        fsm["melt"]();   //solid-liquid
+        // console.log(fsm.state); //liquid
+        // fsm.vaporize(); // liquid -gas
+        // console.log(fsm.state); //gas
+        // fsm.condense(); //gas -liquid
+        // console.log(fsm.state); //liquid
+
+        // fsm.freeze();  //liqiud-solid
+        // console.log(fsm.state); //solid
+    }
+
+
+    test2() {
+        var fsm = new StateMachine({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B' },
+                { name: 'step', from: 'B', to: 'C' },
+                { name: 'step', from: 'C', to: 'D' },
+                { name: 'reset', from: ['B', 'C', 'D'], to: 'A' }
+            ]
+        })
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.reset();
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.reset();
+        //   console.log(fsm.state);
+    }
+
+
+
+    test3() {
+        var fsm = new StateMachine({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B' },
+                { name: 'step', from: 'B', to: 'C' },
+                { name: 'step', from: 'C', to: 'D' },
+                { name: 'reset', from: "*", to: 'A' }
+            ]
+        })
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.reset();
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.step();
+        //   console.log(fsm.state);
+        //   fsm.reset();
+        //   console.log(fsm.state);
+
+
+        //  console.log(fsm.allStates())
+
+    }
+
+
+    test4() {
+        var fsm = new StateMachine({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B' },
+                { name: 'step', from: 'B', to: 'C' },
+                { name: 'step', from: 'C', to: 'D' },
+                { name: 'goto', from: "*", to: function (s) { return s } }
+            ]
+        })
+        // fsm.goto("C");
+        // console.log(fsm.state);
+    }
+
+
+
+
+    test5() {
+        var fsm = new StateMachine({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B' }
+            ],
+            data: {
+                color: 'red',
+                nameStr: "刘鑫"
+            },
+            methods: {
+                describe: function () {
+                    console.log('I am ' + this.color);
+                },
+                liuxin: function () {
+                    console.log("name: " + this.nameStr);
+                }
+            }
+        });
+
+        //   fsm.state;      // 'A'
+        //   fsm.color;      // 'red'
+        //   fsm.describe(); // 'I am red'
+
+
+        // console.log(fsm.nameStr);
+        // fsm.liuxin();  
+    }
+
+
+    test6() {
+        var FSM = StateMachine.factory({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B' }
+            ],
+            data: function (color) {      //  <-- use a method that can be called for each instance
+                return {
+                    color: color
+                }
+            },
+            methods: {
+                describe: function () {
+                    console.log('I am ' + this.color);
+                }
+            }
+        });
+
+        var a = new FSM('red'),
+            b = new FSM('blue');
+
+        a.state; // 'A'
+        b.state; // 'A'
+
+        a.color; // 'red'
+        b.color; // 'blue'
+
+        a.describe(); // 'I am red'
+        b.describe(); // 'I am blue'
+    }
+
+
+
+    //事件
+    test7() {
+        var fsm = new StateMachine({
+            init: 'A',
+            transitions: [
+                { name: 'step', from: 'A', to: 'B',dot:{"falg":1} },
+                { name: 'step', from: 'B', to: 'C',dot:{"falg":2} },
+                { name: 'step', from: 'C', to: 'D' ,dot:{"falg":3}},
+                { name: 'goto', from: "*", to: function (s) { return s } }
+            ],
+            plugins: [
+                new StateMachineHistory()     //  <-- plugin enabled here
+            ]
+        })
+      console.log( StateMachineVisualize(fsm,{ name: 'liuxin', orientation: 'horizontal' }));
+
+
+    //    fsm.observe("onBeforeTransition",function(fift:LiftCycle,p1,p2){
+    //       console.log(fift.transition)
+    //       console.log(fift.from)
+    //       console.log(fift.to)
+    //       console.log(p1);
+    //       console.log(p2)
+    //    })
+    //    fsm.observe("onBeforeTransition",function(fift:LiftCycle,p1,p2){
+    //     console.log(fift.transition)
+    //     console.log(fift.from)
+    //     console.log(fift.to)
+    //     console.log(p1);
+    //     console.log(p2)
+    //  })
+
+        // fsm["step"](1,2);
+        // fsm["goto"]("D",3);
         
-        fsm.freeze();  //liqiud-solid
-        console.log(fsm.state); //solid
-    }
-
-
-    test2(){
-        var fsm = new StateMachine({
-            init: 'A',
-            transitions: [
-              { name: 'step',  from: 'A',               to: 'B' },
-              { name: 'step',  from: 'B',               to: 'C' },
-              { name: 'step',  from: 'C',               to: 'D' },
-              { name: 'reset', from: [ 'B', 'C', 'D' ], to: 'A' }
-            ]
-          })
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.reset();
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.reset();
-          console.log(fsm.state);
+        fsm["step"]();
+        console.log(fsm.history);
+        fsm["step"]();
+        console.log(fsm.history);
+        fsm["step"]();
+        console.log(fsm.history);
+       
+        fsm["goto"]("A");
+        console.log(fsm.history);
+        fsm.historyBack();
+        console.log(fsm.history);
+        fsm.historyForward();
+        console.log(fsm.history);
     }
 
 
 
-    test3(){
-        var fsm = new StateMachine({
-            init: 'A',
-            transitions: [
-              { name: 'step',  from: 'A',               to: 'B' },
-              { name: 'step',  from: 'B',               to: 'C' },
-              { name: 'step',  from: 'C',               to: 'D' },
-              { name: 'reset', from: "*", to: 'A' }
-            ]
-          })
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.reset();
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.step();
-          console.log(fsm.state);
-          fsm.reset();
-          console.log(fsm.state);
 
-    }
-    
-    
+
 }
